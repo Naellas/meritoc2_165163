16f018ddd0f28e0abab54da51d35f3367e55e008
Naellas <maciej.czaplicki98@gmail.com>
2024-04-25 02:45:13 +0200

    Added exercises 32, 25, 46, 52, 66

--- Python Data Type -List - Exercises practice solution.py
+++ Python Data Type -List - Exercises practice solution.py
@@ -91,16 +91,140 @@ def ConvertToString(ListOfCharacters):
     String21 = ''.join(ListOfCharacters)
     return String21
 
-NumberOfItemsInList4 = int(input("How many numbers would you like in your list? "))
-CharacterList21 = [None] * NumberOfItemsInList4
+NumberOfItemsInList5 = int(input("How many numbers would you like in your list? "))
+CharacterList21 = [None] * NumberOfItemsInList5
 
-for n in range(NumberOfItemsInList4):
+for n in range(NumberOfItemsInList5):
     print("Please input ", n+1, " character: ")
     CharacterList21[n] = input()
 
 print("Character list: ", CharacterList21, "has been converted to following string: ", ConvertToString(CharacterList21))
 
-"""
-print("=========================================21==========================================")
-print("Converting a list of characters into a string")
+print("=========================================32==========================================")
+print("Checking if a list contains a sublist")
 print("====================================================================================")
+
+def CheckIfListContainsSublist(ListA, ListB):
+    for i in range(len(ListA)):
+        if ListA[i:i+len(ListB)] == ListB:
+            return True
+    return False
+                
+NumberOfItemsInList6 = int(input("How many numbers would you like in your list? "))
+List32 = [None] * NumberOfItemsInList6
+
+for n in range(NumberOfItemsInList6):
+    print("Please input ", n+1, " character: ")
+    List32[n] = input()
+
+NumberOfItemsInSubList6 = int(input("How many numbers would you like in your sublist? "))
+SubList32 = [None] * NumberOfItemsInSubList6
+
+for n in range(NumberOfItemsInSubList6):
+    print("Please input ", n+1, " character: ")
+    SubList32[n] = input()
+    
+print("Your list: ", List32, ". Provided Sublist: ", SubList32)
+
+if CheckIfListContainsSublist(List32, SubList32) == False:
+    print("Provided sublist is not a sublist of your list")
+else:
+    print("Provided sublist is a sublist of your list")
+
+
+print("=========================================35==========================================")
+print("Create a list by concatenating a given list with range 1 to n")
+print("====================================================================================")
+
+def ConcatenateList(InputList, InputRange):
+    NewList = []
+    for char in InputList:
+        for x in range(InputRange):
+            NewList.append(char + str(x+1))
+    return NewList
+
+NumberOfItemsInList35 = int(input("Please type in amount of characters in your list: "))
+
+List35 = [''] * NumberOfItemsInList35
+
+for n in range(NumberOfItemsInList35):
+    print("Please input ", n+1, " character: ")
+    List35[n] = input()
+
+InputRange = int(input("Please input how many elements should be created for each element in your list: "))
+
+print(ConcatenateList(List35, InputRange))
+
+print("=========================================46==========================================")
+print("Select odd items from the list")
+print("====================================================================================")
+
+def SelectOddNumbers(InputList):
+    NewList = InputList[::2]
+    return NewList
+
+NumberOfItemsInList46 = int(input("Please type in amount of characters in your list: "))
+
+List46 = [''] * NumberOfItemsInList46
+
+for n in range(NumberOfItemsInList46):
+    print("Please input ", n+1, " character: ")
+    List46[n] = input()
+
+print("Odd items within ", List46, " : ", SelectOddNumbers(List46))
+
+print("=========================================52==========================================")
+print("Compute difference between two lists")
+print("====================================================================================")
+
+def ComputeListDifference(list1, list2):
+    List1_List2 = []
+    List2_List1 = []
+    for item in list1:
+        if item not in list2:
+            List1_List2.append(item)
+    for item in list2:
+        if item not in list1:
+            List2_List1.append(item)
+    return [List1_List2, List2_List1]
+
+NumberOfItemsInList1 = int(input("Please type in the number of items in your first list: "))
+List1 = []
+for n in range(NumberOfItemsInList1):
+    print("Please input item", n+1, ": ")
+    List1.append(input())
+
+NumberOfItemsInList2 = int(input("Please type in the number of items in your second list: "))
+List2 = []
+for n in range(NumberOfItemsInList2):
+    print("Please input item", n+1, ": ")
+    List2.append(input())
+
+difference = ComputeListDifference(List1, List2)
+List1_List2 = difference[0]
+List2_List1 = difference[1]
+print("List1 - List2: ", List1_List2)
+print("List2 - List1: ", List2_List1)
+
+"""
+
+print("=========================================66==========================================")
+print("Finding all numbers greater than x within the list")
+print("====================================================================================")
+
+def FindGreaterNumbers(InputList, Number):
+    GreaterList = []
+    for x in range(len(InputList)):
+        if int(InputList[x]) > int(Number):
+            GreaterList.append(InputList[x])
+    return GreaterList
+
+NumberOfItemsInList66 = int(input("Please type in the number of items in your list: "))
+List66 = []
+for n in range(NumberOfItemsInList66):
+    print("Please input item", n+1, ": ")
+    List66.append(input())
+
+Number66 = int(input("Please say above which number you would like to retrieve items from the list: "))
+
+print("Provided list: ", List66, ". Numbers that are greater than ", Number66, "Within that list: ", FindGreaterNumbers(List66, Number66))
